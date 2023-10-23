377f79e4532e1bf9f4e09ea7342c7cf239460174
Jordi Enric <jordi.err@gmail.com>
2023-10-23 10:38:37 +0200

    rm unused dep

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,16 +1,16 @@
-import { createClient } from "@znd/client";
+// import { createClient } from "@znd/client";
 
 export default async function Blog() {
-  const cms = createClient({
-    blogId: "2df4362a-69a1-40a9-8e88-0aec66a70341",
-  });
+  // const cms = createClient({
+  //   blogId: "2df4362a-69a1-40a9-8e88-0aec66a70341",
+  // });
 
-  const posts = await cms.posts.getAll();
+  // const posts = await cms.posts.getAll();
 
   return (
     <div className="p-4">
       <h1 className="font-semibold text-lg">Blog</h1>
-      {JSON.stringify(posts)}
+      {/* {JSON.stringify(posts)} */}
     </div>
   );
 }
